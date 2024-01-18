8c7aade271073febea1af0a395f4c0b07ef513c9
Daniil Byrdin <byrdin.daniel@yandex.ru>
2024-01-18 22:18:57 +0300

    @babel/preset-reac

--- build/main.js
+++ build/main.js
@@ -1,13 +1,14 @@
-function _defineProperty(obj, key, value) { key = _toPropertyKey(key); if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == typeof i ? i : String(i); }
-function _toPrimitive(t, r) { if ("object" != typeof t || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != typeof i) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
-class App {
-  constructor() {
-    _defineProperty(this, "run", async (name = "World") => {
-      console.log(`Htllo ${name}`);
-      console.log([1, 2, 3[(2, 3)]].flat());
-    });
-  }
-}
-const app = new App();
-app.run().then(() => console.log("123")).catch(res => console.log(res));
+// class App {
+//     run = async ( name = "World" ) => {
+//         console.log(`Htllo ${name}`);
+//         console.log( [1,2,3 [2,3]].flat() );
+//     }
+// }
+
+// const app = new App()
+// app.run().then( () => console.log( "123" ) ).catch( res => console.log(res) )
+
+import React from "react";
+import ReactDOM from "react-dom";
+const App = () => /*#__PURE__*/React.createElement("h1", null, "Hello word!");
+ReactDOM.render( /*#__PURE__*/React.createElement(App, null), document.getElementById("root"));
